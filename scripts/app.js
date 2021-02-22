d830005eac48843ec1b9f89093721acd8417d711
Ayo Olawoye <ayo.olawoye@gmail.com>
2021-02-22 03:48:46 +0000

    updated html

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,17 @@
 function init() {
   console.log('CONTENT LOADED')
 
-  const grid = document.querySelector('.grid')
-  console.log(grid)
-  const width = 10
-  const cellCount = width * width
-  const cells = []
 
 
-  // const snakeClass = 'snake'
-  // const snakeStartPosition = 0
-  // let snakeCurrentPosition = 0
 
 
-  // * Make a grid
-  function createGrid() {
-    for (let i = 0; i < cellCount; i++) {
-      const cell = document.createElement('div')
-      cell.textContent = i
-      grid.appendChild(cell)
-      cells.push(cell)
-    }
-    // addSnake(snakeStartPosition)
-  }
 
-  // // * Add Cat to grid
-  // function addsnake(position) {
-  //   cells[position].classList.add(snakeClass)
-  // }
 
-  createGrid(snakeStartPosition)
+
+
+
+
 }
-
-
-
-
-
-
-
-
-
 
 
 window.addEventListener('DOMContentLoaded', init)
