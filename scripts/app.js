81a93af2fa6fa266764bb0bd5d34f450c6912360
Ayo Olawoye <ayo.olawoye@gmail.com>
2021-02-22 09:39:57 +0000

    scores

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,45 @@
 function init() {
-  console.log('CONTENT LOADED')
+
+  const grid = document.querySelector('.grid')
+  const startButton = document.querySelector('.startButton')
+  const resetButton = document.querySelector('.resetButton')
+  const currentScore = document.querySelector('.currentScore')
+
+  let scoreCount = 0
+
+  currentScore.innerHTML = scoreCount
+
+  const width = 10
+  const cellCount = width * width
 
 
 
 
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.innerText = i
+      grid.appendChild(cell)
 
+    }
+  }
 
+  createGrid()
 
+  // function startGame() {
 
+  // }
+
+  // function resetGame() {
+
+  // }
+
+  // function currentScore() {
+
+  // }
+
+  // startButton.addEventListener('click', startGame)
+  // resetButton.addEventListener('click', resetGame)
 
 
 
